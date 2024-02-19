6acc80f3120ad8cefe5f84bde63efcfc45729cc4
Yann <yann.demaux86@gmail.com>
2024-02-19 17:49:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Modal from "./Modal";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  handleShow = () => {
+    this.setState({
+      showModal: true,
+    });
+  };
+
+  handleHide = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  render() {
+    // Condition pour l'affichage de la modal
+    // const modal = this.state.showModal ? (<Modal />) : null;
+    const modal = this.state.showModal && <Modal close={this.handleHide} />;
+
+    return (
+      <div className="App">
+        <button onClick={this.handleShow}>Afficher la Modal</button>
+        {modal}
+      </div>
+    );
+  }
 }
 
 export default App;
